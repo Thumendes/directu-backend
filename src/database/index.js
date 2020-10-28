fd257ad03c9c101a29862367167aba1896afe6c2
Thumendes <62642098+Thumendes@users.noreply.github.com>
2020-10-28 09:24:27 -0300

    Atualização Banco de Dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/directu', { useFindAndModify: false ,useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
